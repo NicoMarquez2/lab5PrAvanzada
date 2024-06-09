e3bb122f6474179ee9e7046730dc2ec386eb7033
NicoMarquez2 <nicolasmarquez005@gmail.com>
2024-06-08 21:11:16 -0300

    creo fabrica

---
+++ fabrica/fabrica.h
@@ -0,0 +1,22 @@
+#ifndef FABRICA_H
+#define FABRICA_H
+
+class fabrica
+{
+private:
+    /* data */
+public:
+    fabrica(/* args */);
+    ~fabrica();
+};
+
+fabrica::fabrica(/* args */)
+{
+}
+
+fabrica::~fabrica()
+{
+}
+
+
+#endif
